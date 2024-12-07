efac006ba333e6349777433884882e1038f48b52
YKDman <dbsrudejr300@gmail.com>
2024-12-07 22:07:13 +0900

    feat : dto type setting

--- src/get-time-slots/dto/request.dto.ts
+++ src/get-time-slots/dto/request.dto.ts
@@ -1,9 +1,28 @@
+import { IsBoolean, IsInt, IsOptional, IsString } from 'class-validator';
+
 export class DayTimetableQueryDto {
+  @IsString()
   start_day_identifier: string;
+
+  @IsString()
   timezone_identifier: string;
+
+  @IsInt()
   service_duration: number;
+
+  @IsOptional()
+  @IsInt()
   days?: number = 1;
+
+  @IsOptional()
+  @IsInt()
   timeslot_interval?: number = 1800;
+
+  @IsOptional()
+  @IsBoolean()
   is_ignore_schedule?: boolean = false;
+
+  @IsOptional()
+  @IsBoolean()
   is_ignore_workhour?: boolean = false;
 }
--- src/get-time-slots/dto/response.dto.ts
+++ src/get-time-slots/dto/response.dto.ts
@@ -1,11 +1,22 @@
-export interface DayTimetable {
-  start_of_day: number; // Unix timestamp
+import { IsBoolean, IsInt } from 'class-validator';
+
+export class DayTimetable {
+  @IsInt()
+  start_of_day: number;
+
+  @IsInt()
   day_modifier: number;
+
+  @IsBoolean()
   is_day_off: boolean;
+
   timeslots: Timeslot[];
 }
 
-export interface Timeslot {
+export class Timeslot {
+  @IsInt()
   begin_at: number;
+
+  @IsInt()
   end_at: number;
 }
