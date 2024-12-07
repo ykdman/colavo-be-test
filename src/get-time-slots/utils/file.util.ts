968ed65f8acdf978dff44f93fe92bea12c25bacc
YKDman <dbsrudejr300@gmail.com>
2024-12-07 22:18:30 +0900

    refactor : loadJson Util Function 예외처리 추가

--- src/get-time-slots/utils/file.util.ts
+++ src/get-time-slots/utils/file.util.ts
@@ -2,6 +2,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 export function loadJson<T>(filePath: string): T {
-  const fullPath = path.resolve(process.cwd(), filePath);
-  return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
+  try {
+    const fullPath = path.resolve(process.cwd(), filePath);
+    return JSON.parse(fs.readFileSync(fullPath, 'utf-8'));
+  } catch {
+    throw new Error(`Failed To Load Json File of ${filePath}`);
+  }
 }
